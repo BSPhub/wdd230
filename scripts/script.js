beb93b1c83064d35090eca5aec8c647d2011fec2
BSPhub <parker57@byui.edu>
2023-01-07 20:42:27 -0700

    home turned in; basic state

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
 'strict mode'
 
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+// section of code for the YEAR
+const options = {year: 'numeric'};
 document.getElementById('date').textContent = new Date().toLocaleDateString('en-US', options);
+
+// section of code for the SITE LAST UPDATED
+let modified = new Date(document.lastModified);
+document.getElementById('updated').textContent = modified;
